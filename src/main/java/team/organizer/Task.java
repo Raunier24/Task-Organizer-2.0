a9dd1517854477858871ad0925f94c0776d8b912
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-06-10 10:40:13 +0200

    dev enter

--- src/main/java/team/organizer/Task.java
+++ src/main/java/team/organizer/Task.java
@@ -1,7 +1,14 @@
-package team.organizer;
+java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Task {
-   
+    private static ArrayList<String> array = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+    private static String input;
     private static void Delete() {
         do {
             array.clear();
